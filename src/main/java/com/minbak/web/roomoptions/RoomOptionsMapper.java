a43f59528c9c733f0d738bcde328de3e8373a1f8
박성철 <imf345666@gmail.com>
2025-02-24 10:24:40 +0900

    편의시설(option) CRUD 재 구현

--- src/main/java/com/minbak/web/roomoptions/RoomOptionsMapper.java
+++ src/main/java/com/minbak/web/roomoptions/RoomOptionsMapper.java
@@ -8,9 +8,29 @@ import java.util.List;
 @Mapper
 public interface RoomOptionsMapper {
 
-    // 사용자가 선택한 편의시설을 포함하는 숙소 리스트 조회
-    List<RoomOptionsDto> getRoomsByAmenities(@Param("amenities") List<String> amenities, @Param("size") int size);
+    // 1. 전체 편의시설 조회
+    List<RoomOptionsDto> findAllRoomOptions();
 
-    //  모든 숙소 조회 (편의시설 필터 없이)
-    List<RoomOptionsDto> getAllRooms();
+    // 2. 특정 ID의 편의시설 조회
+    RoomOptionsDto findRoomOptionById(@Param("optionId") int optionId);
+
+    // 3. 새 편의시설 삽입
+    int createRoomOption(RoomOptionsDto roomOptionsDto);
+
+    // 4. 편의시설 정보 업데이트
+    int updateRoomOption(RoomOptionsDto roomOptionsDto);
+
+    // 5. 편의시설 삭제
+    int deleteRoomOption(@Param("optionId") int optionId);
+
+
+
+
+
+//    다대다 기능으로 필요 없게 됨
+//    //  모든 숙소 조회 (편의시설 필터 없이)
+//    List<RoomOptionsDto> getAllRooms();
+//    // 6. 사용자가 선택한 편의시설을 포함하는 숙소 리스트 조회
+//    List<RoomOptionsDto> getRoomsByAmenities(@Param("amenities") List<String> amenities, @Param("size") int size);
+
 }
